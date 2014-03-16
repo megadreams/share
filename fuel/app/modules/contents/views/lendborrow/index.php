51cbbc627a7316addde8ca5e0bf13d55407f06e4
megadreams <megdreams14@gmail.com>
2014-03-16 12:19:02 +0900

    新規登録画面修正

--- fuel/app/modules/contents/views/lendborrow/index.php
+++ fuel/app/modules/contents/views/lendborrow/index.php
@@ -1,4 +1,34 @@
-    <header class="header ">
+<style>
+.tutorial-top {
+    background-image: url('/assets/img/logo.png');
+    background-repeat: no-repeat;
+    background-position: 50%;
+    height: 150px;
+    padding-top: 150px;
+    font-weight: bold;
+}
+.tutorial-regist {
+    width: 50px;    
+    position: absolute;
+    top: 45px;
+    left: 79%;
+    -webkit-animation: yajirushi-anime 1s cubic-bezier(0.0, 0.4, 0.5, 1.0) -2s infinite alternate;
+}
+
+@-webkit-keyframes yajirushi-anime {
+    0%   {
+        top: 45px;
+        left: 79%;
+    }
+    100% {
+        top: 70px;
+        left: 79%;
+    }
+}
+
+</style>
+
+    <header class="header">        
         <div class="head-left-btn float">
             <a class="lendborrow-add-btn" href="<?php echo $view_data['base_url'] . 'setting/';?>">
                 設定
@@ -9,7 +39,7 @@
         </div>
         <div class="head-right-btn float">
             <a class="lendborrow-add-btn" href="<?php echo $view_data['base_url'] . 'lendborrow/create';?>">
-                追加
+                ＋
             </a>            　
         </div>
     </header>
@@ -49,8 +79,13 @@
                 </a>
             <?php endforeach; ?>
         <?php else: ?>
+            <div class="tutorial-top">
+                登録データはありません。
+            </div>
+            <img class="tutorial-regist" src="/assets/img/common/yajirushi-ue.png">
             <div>
-                現在、貸し借りしている情報はありません。
+                <span class="newline">右上の＋ボタンを押して</span>
+                <span class="newline">かしかり情報を登録して下さい</span>
             </div>
         <?php endif;?>
     </section>
